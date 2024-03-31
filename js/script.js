2ea1fff7010bd4c8114ae2db3d8e3e5fc775a4cb
conner12345678 <cehly065@west-mec.org>
2024-03-30 19:01:07 -0700

    added js changes(does not work yet)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-// const submit = () => {
-    
-// }
+const submit = () => {
+    const container = document.createElement("div")
+    const header = document.createElement("h1")
+    header.innerHTML = document.getElementById("cmpyName").value
+    const type = document.createElement('h5')
+    type.innerHTML = document.createElement("cmpyType").value
+    const desc = document.createElement('p')
+    desc.innerHTML = document.getElementById('cmpyDescription').value
+    container.innerHTML = header + type + desc
+    document.getElementById("theList").appendChild(container)
+}
