8741bdde48dddfd55effdeeedfd4517deec4ccb7
conner12345678 <cehly065@west-mec.org>
2024-04-05 21:22:21 -0700

    fully functional js
    I fixed it now it works this took weeks yay

--- js/load.js
+++ js/load.js
@@ -1,6 +1,13 @@
 function loadData(){
     let queryString = window?.location.search
-    var finale =JSON.parse('{"' + queryString.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
+    var finale =JSON.parse('{"' + queryString.replace(/&/g, '","').replace(/=/g,'":"').replace(/\?/g,'') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
     console.log(finale)
+    const title = finale.cmpyName
+    const type = finale.cmpyType
+    const desc = finale.cmpyDescription
+    const container = document.createElement('div')
+    container.innerHTML = `<h1>${title}</h1>
+    <p style='color: white;'>${desc}</p>`
+    document.getElementById('theList').appendChild(container)
 }
 loadData()
